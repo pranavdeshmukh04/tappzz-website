30b0ab66c1df87c0df4c9b8a032a0da7c611dbe5
Pranav Deshmukh <pranavmdeshmukh047@gmail.com>
2022-11-17 15:36:59 +0530

    Created data.js and added data

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "FASHION CARNIVAL",
+      desc: "STAY CLASSY ON A BUDGET! GET FLAT 50% - 80% OFF!",
+      bg: "A1D6E2",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/gwhGXNc/slideimg6.png",
+      title: "BEST OF BRANDS",
+      desc: "FASHION IS IN THE AIR0! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "BCBABE",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/vkkKMHL/slideimg3.png",
+      title: "WARM WINTER COLLECTION",
+      desc: "STAY IN STYLE WITH US! GET FLAT 45% OFF FOR WINTER COLLECTION.",
+      bg: "1995AD",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/GTNhKZM/pexels-kafeel-ahmed-3998154.jpg",
+      title: "ETHNIC WEAR",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/sV8QWwz/logan-weaver-lgnwvr-Gi1r-Dqy9-R40-unsplash.jpg",
+      title: "CASUAL WEAR",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/SJJRTGq/mark-adriane-x-Qgh-SLXYD3-M-unsplash.jpg",
+      title: "ACTIVE WEAR",
+    },
+    {
+      id: 4,
+      img: "https://i.ibb.co/WFTNG75/ins-vikrant-f-Ds9-Mu-EJ4gc-unsplash.jpg",
+      title: "WESTERN WEAR",
+    },
+    {
+      id: 5,
+      img: "https://i.ibb.co/StbnDf3/pexels-oliver-sj-str-m-1103828.jpg",
+      title: "SPORTS WEAR",
+    },
+    {
+      id: 6,
+      img: "https://i.ibb.co/WpZSLSF/pexels-sam-lion-6001444.jpg",
+      title: "LOUNGE WEAR",
+    },
+    {
+      id: 7,
+      img: "https://i.ibb.co/ZB7DxbB/pylyp-sukhenko-Srs-IBi-JPpxs-unsplash.jpg",
+      title: "FORMAL WEAR",
+    },
+    {
+      id: 8,
+      img: "https://i.ibb.co/Cmyhqt7/daiga-ellaby-JZ51o-UOY8-unsplash.jpg",
+      title: "KIDS WEAR",
+    }
+  ];
