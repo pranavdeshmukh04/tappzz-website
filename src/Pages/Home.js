6189b61316c9be271bdc485f854a4cb2f4b22f99
Pranav Deshmukh <pranavmdeshmukh047@gmail.com>
2022-11-17 15:37:53 +0530

    Created and Implemented CategoryItem.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
     <div>
       <Announcement/>
       <Navbar/>
+      <Slider/>
+      <Categories/>
       <Footer/>
     </div>
   )
