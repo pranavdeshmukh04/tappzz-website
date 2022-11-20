8c056fc4e19443b7f092cc6316c1ecd319d30820
Pranav Deshmukh <pranavmdeshmukh047@gmail.com>
2022-11-20 14:30:19 +0530

    Added marquee effect to Announcement.js

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Marquee from'react-fast-marquee'
 
 const Container = styled.div`
     height: 30px;
@@ -9,12 +10,13 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
     font-size: 16px;
-
 `
 const Announcement = () => {
   return (
     <Container>
-        Hurry! Avail Today's Offer! Flat 20% Off Discount [ Use Coupon Code : TAPPZZ20 ]
+      <Marquee speed={100} gradient={false}>
+        HURRY! Avail Today's Offer! Flat 20% Off Discount [ Use Coupon Code : TAPPZZ20 ]
+      </Marquee>
     </Container>
   )
 }
